0f887efdc6219de31a4da3664d4dd8f6dfa73971
witalonrodrigues <witalonar@outlook.com>
2026-08-24 22:11:58 -0300

    feat: adicionando classe inicial

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿import java.util.Scanner;
+
+public class CineCidade
+{
+    public static void main(String[] args)
+    {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nome do cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        scanner.close();
+    }
+}
